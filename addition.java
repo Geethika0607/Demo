e72c75184d8c202fb2c487811373db092d91f4c0
Geethika0607 <ammugeeth8@gmail.com>
2025-01-27 18:10:42 +0530

    Update addition.java

--- addition.java
+++ addition.java
@@ -1,12 +1,7 @@
 // App.java
 // This is a test commit
 public class addition{
-
-    // Method to add two numbers
-   
-
-    
-    public static void main(String[] args) {
+     public static void main(String[] args) {
        
        int a = 10;
        int b = 19;
