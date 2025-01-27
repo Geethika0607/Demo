c3c8937a9028982b96e57d6db3944015700f6be3
geethika <ammugeeth8@gmail.com>
2025-01-27 18:02:23 +0530

    second messge

--- addition.java
+++ addition.java
@@ -1,24 +1,18 @@
 // App.java
 // This is a test commit
-public class App {
+public class addition{
 
     // Method to add two numbers
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
    
-    public static void testAdd() {
-        // Assertions to check if add() works correctly
-        assert add(1, 2) == 3 : "Test Case 1 Failed";
-        assert add(1, -1) == 0 : "Test Case 2 Failed";
-        System.out.println("All Test Cases Passed!");
-    }
 
     
     public static void main(String[] args) {
-        // Run the test cases
-        testAdd();
+       
+       int a = 10;
+       int b = 19;
+       int c;
+       c = a + b;
+       System.out.println("addition of two numbers:"+c );
     }
 }
 
